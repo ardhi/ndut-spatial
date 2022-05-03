c1c944ce7ea79925cbdedd5658bd44b320f5c70e
Ardhi Lukianto <ardhi@lukianto.com>
2022-05-03 20:54:47 +0700

    Updates

---
+++ ndutDb/feature/lat-lng.js
@@ -0,0 +1,14 @@
+module.exports = {
+  properties: {
+    lat: {
+      type: Number,
+      required: false,
+      index: true
+    },
+    lng: {
+      type: Number,
+      required: false,
+      index: true
+    }
+  }
+}
